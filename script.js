f3ac3a2c48604e66e74352496a0a7613cddb8cea
UlugBek <beka.eco.95@gmail.com>
2020-07-02 20:58:51 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,383 @@
+!function () {
+
+  var today = moment();
+  var legendCheck = false;
+
+  function Calendar(selector, events) {
+    this.el = document.querySelector(selector);
+    this.events = events;
+    this.current = moment().date(1);
+    this.draw();
+    var current = document.querySelector('.today');
+    if (current) {
+      var self = this;
+      window.setTimeout(function () {
+        self.openDay(current);
+      }, 500);
+    }
+  }
+
+  Calendar.prototype.draw = function () {
+    //Create Header
+    this.drawHeader();
+
+    //Draw Month
+    this.drawMonth();
+
+    if(!legendCheck) {
+      this.drawLegend();
+      legendCheck = true;
+    }
+
+    document.querySelectorAll('.day-events .green').forEach(e => {
+      e.parentElement.parentElement.querySelector('.day-number').style.color = 'rgba(153, 198, 109, 1)';
+    });
+
+    document.querySelectorAll('.day-events .red').forEach(e => {
+      e.parentElement.parentElement.querySelector('.day-number').style.color = '#E20613';
+    });
+
+    document.querySelectorAll('.day-events .orange').forEach(e => {
+      e.parentElement.parentElement.querySelector('.day-number').style.color = 'rgba(247, 167, 0, 1)';
+    });
+
+    document.querySelectorAll('.day').forEach(d => {
+      d.addEventListener('click', function(e) {
+        document.querySelectorAll('.day').forEach(x => {
+          x.classList.remove('active');
+        })
+        e.target.classList.add('active');
+      });
+    })
+  }
+
+  Calendar.prototype.drawHeader = function () {
+    var self = this;
+    if (!this.header) {
+      //Create the header elements
+      this.header = createElement('div', 'header');
+      this.header.className = 'header';
+
+      this.title = createElement('h1');
+
+      var right = createElement('div', 'right');
+      right.addEventListener('click', function () { self.nextMonth(); });
+
+      var left = createElement('div', 'left');
+      left.addEventListener('click', function () { self.prevMonth(); });
+
+      //Append the Elements
+      this.header.appendChild(this.title);
+      this.header.appendChild(right);
+      this.header.appendChild(left);
+      this.el.appendChild(this.header);
+    }
+
+    this.title.innerHTML = this.current.format('MMMM YYYY');
+  }
+
+  Calendar.prototype.drawMonth = function () {
+    var self = this;
+
+    this.events.forEach(function (ev) {
+      ev.date = self.current.clone().date(ev.eventDate.split('/')[0]);
+    });
+
+
+    if (this.month) {
+      this.oldMonth = this.month;
+      this.oldMonth.className = 'month out ' + (self.next ? 'next' : 'prev');
+      this.oldMonth.addEventListener('webkitAnimationEnd', function () {
+        self.oldMonth.parentNode.removeChild(self.oldMonth);
+        self.month = createElement('div', 'month');
+        self.backFill();
+        self.currentMonth();
+        self.fowardFill();
+        self.el.appendChild(self.month);
+        window.setTimeout(function () {
+          self.month.className = 'month in ' + (self.next ? 'next' : 'prev');
+        }, 16);
+      });
+    } else {
+      this.month = createElement('div', 'month');
+      this.el.appendChild(this.month);
+      this.backFill();
+      this.currentMonth();
+      this.fowardFill();
+      this.month.className = 'month new';
+    }
+  }
+
+  Calendar.prototype.backFill = function () {
+    var clone = this.current.clone();
+    var dayOfWeek = clone.day();
+
+    if (!dayOfWeek) { return; }
+
+    clone.subtract('days', dayOfWeek + 1);
+
+    for (var i = dayOfWeek; i > 0; i--) {
+      this.drawDay(clone.add('days', 1));
+    }
+  }
+
+  Calendar.prototype.fowardFill = function () {
+    var clone = this.current.clone().add('months', 1).subtract('days', 1);
+    var dayOfWeek = clone.day();
+
+    if (dayOfWeek === 6) { return; }
+
+    for (var i = dayOfWeek; i < 6; i++) {
+      this.drawDay(clone.add('days', 1));
+    }
+  }
+
+  Calendar.prototype.currentMonth = function () {
+    var clone = this.current.clone();
+
+    while (clone.month() === this.current.month()) {
+      this.drawDay(clone);
+      clone.add('days', 1);
+    }
+  }
+
+  Calendar.prototype.getWeek = function (day) {
+    if (!this.week || day.day() === 0) {
+      this.week = createElement('div', 'week');
+      this.month.appendChild(this.week);
+    }
+  }
+
+  Calendar.prototype.drawDay = function (day) {
+    var self = this;
+    this.getWeek(day);
+
+    //Outer Day
+    var outer = createElement('div', this.getDayClass(day));
+    outer.addEventListener('click', function () {
+      self.openDay(this);
+    });
+
+    //Day Name
+    var name = createElement('div', 'day-name', day.format('ddd'));
+
+    //Day Number
+    var number = createElement('div', 'day-number', day.format('DD'));
+
+
+    //Events
+    var events = createElement('div', 'day-events');
+    this.drawEvents(day, events);
+
+    outer.appendChild(name);
+    outer.appendChild(number);
+    outer.appendChild(events);
+    this.week.appendChild(outer);
+  }
+
+  Calendar.prototype.drawEvents = function (day, element) {
+    if (day.month() === this.current.month()) {
+      var todaysEvents = this.events.reduce(function (memo, ev) {
+        if (ev.date.isSame(day, 'day')) {
+          memo.push(ev);
+        }
+        return memo;
+      }, []);
+
+      todaysEvents.forEach(function (ev) {
+        var evSpan = createElement('span', ev.color);
+        element.appendChild(evSpan);
+      });
+    }
+  }
+
+  Calendar.prototype.getDayClass = function (day) {
+    classes = ['day'];
+    if (day.month() !== this.current.month()) {
+      classes.push('other');
+    } else if (today.isSame(day, 'day')) {
+      classes.push('today', 'active');
+    }
+    return classes.join(' ');
+  }
+
+  Calendar.prototype.openDay = function (el) {
+    var details, arrow;
+    var dayNumber = +el.querySelectorAll('.day-number')[0].innerText || +el.querySelectorAll('.day-number')[0].textContent;
+    var day = this.current.clone().date(dayNumber);
+
+    var currentOpened = document.querySelector('.details');
+
+    //Check to see if there is an open detais box on the current row
+    if (currentOpened && currentOpened.parentNode === el.parentNode) {
+      details = currentOpened;
+      arrow = document.querySelector('.arrow');
+    } else {
+      //Close the open events on differnt week row
+      //currentOpened && currentOpened.parentNode.removeChild(currentOpened);
+      if (currentOpened) {
+        currentOpened.addEventListener('webkitAnimationEnd', function () {
+          currentOpened.parentNode.removeChild(currentOpened);
+        });
+        currentOpened.addEventListener('oanimationend', function () {
+          currentOpened.parentNode.removeChild(currentOpened);
+        });
+        currentOpened.addEventListener('msAnimationEnd', function () {
+          currentOpened.parentNode.removeChild(currentOpened);
+        });
+        currentOpened.addEventListener('animationend', function () {
+          currentOpened.parentNode.removeChild(currentOpened);
+        });
+        currentOpened.className = 'details out';
+      }
+
+      //Create the Details Container
+      details = createElement('div', 'details in');
+
+      //Create the arrow
+      var arrow = createElement('div', 'arrow');
+
+      //Create the event wrapper
+
+      details.appendChild(arrow);
+      el.parentNode.appendChild(details);
+    }
+
+    var todaysEvents = this.events.reduce(function (memo, ev) {
+      if (ev.date.isSame(day, 'day')) {
+        memo.push(ev);
+      }
+      return memo;
+    }, []);
+
+    this.renderEvents(todaysEvents, details);
+
+    arrow.style.left = el.offsetLeft - el.parentNode.offsetLeft + 70 + 'px';
+  }
+
+  Calendar.prototype.renderEvents = function (events, ele) {
+    //Remove any events in the current details element
+    var currentWrapper = ele.querySelector('.events');
+    var wrapper = createElement('div', 'events in' + (currentWrapper ? ' new' : ''));
+
+    events.forEach(function (ev) {
+      var eventTime = createElement('div', 'eventTime', ev.time);
+      var div = createElement('div', 'event');
+      var div1 = createElement('div', 'div1');
+      var div2 = createElement('div', 'div2');
+      var square = createElement('div');
+      var span = createElement('span', '', ev.eventName);
+      var location = createElement('div', 'eventLocation', ev.location);
+      var icon = createElement('img');
+      icon.setAttribute("src", `img/${ev.live ? 'live' : 'icon'}.png`);
+      var iconText = createElement('p');
+      iconText.innerHTML = ev.live ? "Live now" : "Virtual Class";
+      var button = createElement('a', 'eventButton');
+      button.setAttribute('href', ev.link);
+      button.innerHTML = 'Click here to view Course Materials.';
+
+      div2.appendChild(icon);
+      div2.appendChild(iconText);
+      div1.appendChild(square);
+      div1.appendChild(eventTime);
+      div1.appendChild(span);
+      div1.appendChild(location);
+      div1.appendChild(button);
+      div.appendChild(div1);
+      div.appendChild(div2);
+      wrapper.appendChild(div);
+    });
+
+    if (!events.length) {
+      var div = createElement('div', 'event empty');
+      var span = createElement('span', '', 'No Events');
+
+      div.appendChild(span);
+      wrapper.appendChild(div);
+    }
+
+    if (currentWrapper) {
+      currentWrapper.className = 'events out';
+      currentWrapper.addEventListener('webkitAnimationEnd', function () {
+        currentWrapper.parentNode.removeChild(currentWrapper);
+        ele.appendChild(wrapper);
+      });
+      currentWrapper.addEventListener('oanimationend', function () {
+        currentWrapper.parentNode.removeChild(currentWrapper);
+        ele.appendChild(wrapper);
+      });
+      currentWrapper.addEventListener('msAnimationEnd', function () {
+        currentWrapper.parentNode.removeChild(currentWrapper);
+        ele.appendChild(wrapper);
+      });
+      currentWrapper.addEventListener('animationend', function () {
+        currentWrapper.parentNode.removeChild(currentWrapper);
+        ele.appendChild(wrapper);
+      });
+    } else {
+      ele.appendChild(wrapper);
+    }
+  }
+
+  Calendar.prototype.drawLegend = function () {
+    var legend = createElement('div', 'legend');
+    var calendars = this.events.map(function (e) {
+      return e.calendar + '|' + e.color;
+    }).reduce(function (memo, e) {
+      if (memo.indexOf(e) === -1) {
+        memo.push(e);
+      }
+      return memo;
+    }, []).forEach(function (e) {
+      var parts = e.split('|');
+      var entry = createElement('span', 'entry ' + parts[1], parts[0]);
+      legend.appendChild(entry);
+    });
+    this.el.appendChild(legend);
+  }
+
+  Calendar.prototype.nextMonth = function () {
+    this.current.add('months', 1);
+    this.next = true;
+    this.draw();
+  }
+
+  Calendar.prototype.prevMonth = function () {
+    this.current.subtract('months', 1);
+    this.next = false;
+    this.draw();
+  }
+
+  window.Calendar = Calendar;
+
+  function createElement(tagName, className, innerText) {
+    var ele = document.createElement(tagName);
+    if (className) {
+      ele.className = className;
+    }
+    if (innerText) {
+      ele.innderText = ele.textContent = innerText;
+    }
+    return ele;
+  }
+}();
+
+!function () {
+  var data = [
+    { eventName: 'Decision Making & Problem Solving', eventDate: '6/06/2020', calendar: 'Events', color: 'orange', time: '10:00am — 5:00pm', location: 'The Plaza, Limkokwing University', live: true, link: '#' },
+
+    { eventName: 'Decision Making & Problem Solving', eventDate: '16/07/2020', calendar: 'Holidays', color: 'red', time: '10:00am — 5:00pm', location: 'The Plaza, Limkokwing University', live: true, link: '#' },
+    { eventName: 'Decision Making & Problem Solving', eventDate: '16/07/2020', calendar: 'Holidays', color: 'red', time: '10:00am — 5:00pm', location: 'The Plaza, Limkokwing University', live: false, link: '#' },
+
+    { eventName: 'Decision Making & Problem Solving', eventDate: '9/08/2020', calendar: 'Exams', color: 'green', time: '10:00am — 5:00pm', location: 'The Plaza, Limkokwing University', live: false, link: '#' },
+  ];
+
+
+
+  function addDate(ev) {
+
+  }
+
+  var calendar = new Calendar('#calendar', data);
+
+}();
